6f4b0c582a6653a2021b35d2e75b7ac60c985cb2
Lukas Treffner <lukas.treffner@gmx.at>
2023-05-09 19:42:00 +0200

    test with _redirects on netlify

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,27 +1,5 @@
 <template>
-  <h1 id="title" v-if="this.showTitle">Inventory Hero</h1>
-  <h1 id="homeTitle" >Home</h1>
-  <Slide @openMenu="this.showTitle=false" @closeMenu="this.showTitle=true" :crossIcon="false" :closeOnNavigation="true" enableOutsideClick>
-    <a id="home" href="#">
-      <span>Home</span>
-    </a>
-    <a href="">
-      <span>
-        Boxes
-      </span>
-    </a>
-    <a href="">
-      <span>
-        Locations
-      </span>
-    </a>
-    <a href="">
-      <span>
-        Products
-      </span>
-    </a>
-  </Slide>
-
+  <SandwichMenu/>
   <add-button />
   <qr-button />
 </template>
@@ -29,12 +7,12 @@
 <script>
 import AddButton from '@/components/AddButton.vue'
 import QrButton from '@/components/QrButton.vue'
-import { Slide } from 'vue3-burger-menu';
+import SandwichMenu from "@/components/SandwichMenu.vue";
 
 export default {
   name: 'App',
   components: {
-    Slide,
+    SandwichMenu,
     AddButton,
     QrButton
   },
@@ -54,24 +32,5 @@ a:visited {
   color: white;
 }
 <style>
-#title {
-  position: absolute;
-  top: 15px;
-  left: 50%;
-  transform: translateX(-50%);
-}
-#homeTitle {
-  position: absolute;
-  top: 10vh;
-  left: 50%;
-  transform: translateX(-50%);
-}
 
-.bm-menu {
-      background-color: rgb(0, 0, 0); /* Black*/
-    }
-
-.bm-burger-bars {
-  background-color: white;
-}
 </style>
