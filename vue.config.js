a7f977e7cbd63525710b4a632dc6374ac702c80b
Lukas Treffner <lukas.treffner@gmx.at>
2023-06-21 18:57:37 +0200

    vue config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  pwa: {
+    name: 'InventoryHero',
+    themeColor: '#d1bdff',
+    msTileColor: '#d1bdff',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: '#d1bdff',
+    workboxOptions: {
+      skipWaiting: true
+    }
+  },
 
   pluginOptions: {
     vuetify: {
